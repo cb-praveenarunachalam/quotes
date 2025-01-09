2d2c076a19ba4a76819215e322c9677205e6a6ee
cb-ramk <ram.k@chargebee.com>
2025-01-09 20:48:33 +0530

    cors

--- src/main/java/com/vilai/quote/controller/HelloWorldController.java
+++ src/main/java/com/vilai/quote/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://3b91-14-142-185-230.ngrok-free.app", "http://10.2.236.142:5173"}, allowedHeaders = "*", methods = {RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*", methods = {RequestMethod.HEAD, RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, allowCredentials = "true")
 public class HelloWorldController {
 
 	@GetMapping("/hello")
